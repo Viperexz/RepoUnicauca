335c91812a63ce8a9104b9b71289fce8d5bcdfab
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2024-10-03 22:48:24 -0500

    Re organizacion Software 3

--- Software 3/Practica CSS/Taller/Pagina Personal Estu/scripts/carrousel.js
+++
@@ -1,8 +0,0 @@
-let slides = Array.from(document.querySelectorAll('.slide'));
-let currentSlide = 0;
-
-setInterval(() => {
-    slides[currentSlide].style.transform = 'translateX(-100%)';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.transform = 'translateX(0)';
-}, 3000); // Cambia las diapositivas cada 3 segundos
